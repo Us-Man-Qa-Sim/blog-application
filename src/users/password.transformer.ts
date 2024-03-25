344ed7089653c31d7492b09e59f72f90ceba909d
Us-Man-Qa-Sim <usmanqasim0900@gmail.com>
2024-03-26 02:59:43 +0500

    Added encription to the user password while seeding user data

--- src/users/password.transformer.ts
+++ src/users/password.transformer.ts
@@ -1,9 +1,10 @@
 import { ValueTransformer } from 'typeorm';
+import { UserUtils } from './../utils/user.utils';
 
 export class PasswordTransformer implements ValueTransformer {
   to(value: string) {
     if (value) {
-      return value + 123;
+      return UserUtils.generateHash(value);
     } else {
       return null;
     }
---
+++ src/utils/user.utils.ts
@@ -0,0 +1,11 @@
+import * as bcrypt from 'bcrypt';
+
+export class UserUtils {
+  public static generateHash(str: string): string {
+    return bcrypt.hashSync(str, parseInt(process.env.BCRYPT_WORK));
+  }
+
+  public static trimLowerString(str: string): string {
+    return str.trim().toLowerCase();
+  }
+}
